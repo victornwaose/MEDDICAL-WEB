af5ad635715951a5fcac6bbf47f6d44338779c4a
nwaose victor <nwaosevictor1234@gmail.com>
2022-01-04 19:21:54 +0100

    added changes

--- src/components/our_doctor/OurDoctor.jsx
+++ src/components/our_doctor/OurDoctor.jsx
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import { FaInstagramSquare, FaLinkedin, FaFacebook } from "react-icons/fa";
+
+import "./OurDoctor.css";
+import { data } from "../../data/Data";
 
 const OurDoctor = () => {
+    const [carousels, setCarousels] = useState(data);
+    console.log(carousels);
+
+    const item = carousels.map((carousel) => {
+        return (
+            <div className="carousel">
+                <div className="carousel-profiles">
+                    <img src={carousel?.images} alt="img" />
+                    <div className="carousel_info">
+                        <h2>{carousel?.doctorName}</h2>
+                        <h4>{carousel?.skills}</h4>
+                        <div className="carousel_social">
+                            <FaLinkedin />
+                            <FaFacebook />
+                            <FaInstagramSquare />
+                        </div>
+                    </div>
+                    <div className="carousel_moreDetail">
+                        <h4>view more</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    });
+    const responsive = {
+        0: {
+            items: 1,
+        },
+        512: {
+            items: 2,
+        },
+        790: {
+            items: 3,
+        },
+    };
+
     return (
-        <div>
-            <h4>TRUSTED CARE </h4>{" "}
-            <h2>Our Doctor </h2>
+        <div className="ourDoctor">
+            <div className="ourDoctor_heading">
+                <h4>TRUSTED CARE </h4>
+                <h2>Our Doctor </h2>
+            </div>
+            <AliceCarousel
+                mouseTracking
+                keyboardNavigation8
+                autoPlayInterval={1000}
+                animationDuration={1500}
+                disableButtonsControls
+                responsive={responsive}
+                items={item}
+            />
         </div>
     );
 };
---
+++ src/data/Data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        images: "/images/doc.jpg",
+        doctorName: "Segun Ibrahim",
+        skills: "Neurology",
+    },
+    {
+        images: "/images/doc.jpg",
+        doctorName: "Nkeze Sylvester",
+        skills: "Endocrinology",
+    },
+    {
+        images: "/images/doc.jpg",
+        doctorName: "Nkeze Sylvester",
+        skills: "ENT",
+    },
+];
